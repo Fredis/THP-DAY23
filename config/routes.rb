81d04efff2b15e0cc05bfd31c044c33ee1eae1fa
Apelerin <arcanynka@gmail.com>
2018-11-01 15:06:47 +0100

    Delete shitty migration from JUliette <3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  resources 'gossips', only: [:new, :create, :show, :index, :edit, :update, :destroy]
-  resources 'registrations', only: [:new, :create]
+  resources 'registrations', only: [:new, :create] do
+     resources 'gossips', only: [:new, :create, :show, :index, :edit, :update, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20181101120759_delete_column_anymous_user.rb
+++
@@ -1,5 +0,0 @@
-class DeleteColumnAnymousUser < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :gossips, :anonymous_gossipeur
-  end
-end
