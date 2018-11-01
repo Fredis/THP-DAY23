74b50b2c18b20b4c98b103181dcf6d5b7dbd4f5e
Juliette Audema <juliette.audema@gmail.com>
2018-11-01 13:23:36 +0100

    Modif table tamère

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,8 +1,11 @@
 class Registration < ApplicationRecord
   def Registration.check(registration)
-      if registration.password == User.find_by(email: "#{registration.email}").password
-        return true
-      end
+    if User.find_by(email: "#{registration.email}") == nil
+      return false 
+    end 
+    if registration.password == User.find_by(email: "#{registration.email}").password
+      return true
+    end
     return false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_01_110602) do
+ActiveRecord::Schema.define(version: 2018_11_01_120948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "gossips", force: :cascade do |t|
-    t.bigint "user_id"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_gossips_on_user_id"
   end
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2018_11_01_110602) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "gossips", "users"
   add_foreign_key "likes", "gossips"
   add_foreign_key "likes", "users"
 end
